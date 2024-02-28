61f0e96ce70342450993f4fc94ea9151d3a6bcad
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-02-28 08:41:30 +0700

    backend_user_models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRotes from './routes/user'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -9,10 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use("/api/users", userRotes);
 
-app.get("/api/test", async (req: Request, res: Response)=>{
-    res.json({message:"Hello from express"});
-});
+
 
 app.listen(4000, ()=>{
     console.log("server is running at localhost:4000")
